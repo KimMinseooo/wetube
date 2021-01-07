2551f397c83d33b414794310be5428fe5542e520
Minseo Kim <minseo.dev@gmail.com>
2021-01-07 10:45:39 +0900

    Added Controller in MVC Pattern

--- app.js
+++ app.js
@@ -4,18 +4,11 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
-import { userRouter } from "./router";
+import userRouter  from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
+import routes from "./routes";
 const app = express();
-
-const handleHome = (req, res) => res.send("Hello from ass ");
-
-const handleProfile = (req, res) => res.send("You are on my profile");
-
-const betweenHome = (req, res, next) => {
-    console.log("Between");
-    next();
-    // next()가 꼭 필요 ! 하지않으면 다음에 실행할 콜백 함수가 실행하지않음.
-}; 
 
 //middleware의 위치가 중요하다 
 //route를 처리하기 전에 위치해야함
@@ -25,11 +18,10 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(helmet());
 app.use(morgan("dev"));
 
-app.get("/", handleHome);
-
-app.get("/profile", handleProfile);
 
 //누군가 /user경로에 접속하면 이 router 전체를 사용하겠다
-app.use("/user", userRouter);
+app.use(routes.home,globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos,videoRouter);
 
 export default app;
