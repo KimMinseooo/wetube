76b49ce72cdef319457d97248eef6332330d044a
Minseo Kim <minseo.dev@gmail.com>
2021-01-08 13:42:08 +0900

    Added videos html

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,5 +1,18 @@
 import express from "express";
+import routes from "../routes";
+import {  
+    videos,
+    upload,
+    videoDetail,
+    editVideo,
+    deleteVideo    
+} from "../controllers/videoController";
 
 const videoRouter = express.Router();
+
+videoRouter.get(routes.upload,upload);
+videoRouter.get(routes.editVideo,editVideo);
+videoRouter.get(routes.delelteVideo,deleteVideo);
+videoRouter.get(routes.videoDetail,videoDetail);
 
 export default videoRouter;
