696e1d16ee8ecd84d0d2b1a4b8b89f545736fbae
Minseo Kim <minseo.dev@gmail.com>
2021-02-18 17:23:35 +0900

    Created Github LogIn & User Profile

--- passport.js
+++ passport.js
@@ -1,8 +1,21 @@
 import passport from "passport";
+import GithubStrategy from "passport-github";
+import { githubLoginCallback } from "./controllers/userController";
 import User from "./models/User";
+import routes from "./routes";
 
 //pssport 에게 strateg를 쓰라고 말함
 passport.use(User.createStrategy());
+
+passport.use(
+    new GithubStrategy({
+    clientID: process.env.GH_ID ,
+    clientSecret: process.env.GH_SECRET ,
+    callbackURL : `http://localhost:4000${routes.gitHubCallback}`  
+       },
+       githubLoginCallback
+        )
+    );
 
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
