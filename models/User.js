ab3f62f7ae9a9b90d47748fd559855aea9f50050
Minseo Kim <minseo.dev@gmail.com>
2021-02-24 17:05:06 +0900

    Created Adding Creator to Video

--- models/User.js
+++ models/User.js
@@ -6,7 +6,19 @@ const UserSchema = new mongoose.Schema({
     email : String,
     avatarUrl : String,
     facebookId : Number,
-    githubId : Number 
+    githubId : Number ,
+    comments : [
+        {
+        type :mongoose.Schema.Types.ObjectId,
+        ref:"Comment"
+        }
+    ],
+    videos : [
+        {
+        type :mongoose.Schema.Types.ObjectId,
+        ref:"Video"
+        }
+    ]
 });
 
 //Schema 추가 사용자인증을 사용하기 위함
